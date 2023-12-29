0018b25156ad60804a002e0831597d9a58cbbff9
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-12-29 16:13:08 +0100

    About screen viewmodel added

--- app/src/main/java/com/example/fruitapplication/ui/aboutScreen/AboutScreen.kt
+++ app/src/main/java/com/example/fruitapplication/ui/aboutScreen/AboutScreen.kt
@@ -1,35 +1,26 @@
 package com.example.fruitapplication.ui.aboutScreen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.fruitapplication.R
 import com.example.fruitapplication.ui.components.FruitImageRow
 
 @Composable
 fun DetailScreen(
+    aboutViewModel: AboutViewModel = viewModel(),
     innerPadding: PaddingValues,
 ) {
     About(
