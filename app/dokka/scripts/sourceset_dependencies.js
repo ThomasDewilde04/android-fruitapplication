9a1988e733dfd0b3d252ebf1ca7b3b9891485ab9
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-12-30 14:18:53 +0100

    Added dokka documentation

---
+++ app/dokka/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":app:dokkaHtml/debug":[],":app:dokkaHtml/generatedByKspDebugKotlin":[],":app:dokkaHtml/generatedByKspReleaseKotlin":[],":app:dokkaHtml/main":[],":app:dokkaHtml/release":[]}'
